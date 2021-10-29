b539c62eef04ed2d4f30f8659779cce7c1a6f926
ou38817732 <ou38817732@gmail.com>
2021-10-29 22:58:02 +0800

    444

---
+++ example/7-4/demo.js
@@ -0,0 +1,7 @@
+var math1= 2+5; // addition
+var math2= 3 * 4.5; //* muitiplaying
+var math3= 1 - 5; //- substracting
+var math4= 20 / 5; // dividing
+
+var math5=1 - 5 * 20; // multiply first
+console.log(math5)
