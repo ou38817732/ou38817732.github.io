069e2c22391fe7fd458b643e0b74d18ca3d035e3
ou38817732 <ou38817732@gmail.com>
2021-10-29 23:02:26 +0800

    Update demo.js

--- example/7-4/demo.js
+++ example/7-4/demo.js
@@ -4,6 +4,7 @@ var math3= 1 - 5; //- substracting
 var math4= 20 / 5; // dividing
 
 var math5=1 - 5 * 20; // multiply first
-console.log(math5)
+//console.log(math5)
 
 var calc = math4 + math5; // Add math4 to math5 and store the result in the variable 'calc'
+// calc= calculation
