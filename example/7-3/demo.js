fb88063701c030b2b4b98287eafb457a1396396d
ou38817732 <ou38817732@gmail.com>
2021-10-29 22:43:01 +0800

    Update demo.js

--- example/7-3/demo.js
+++ example/7-3/demo.js
@@ -13,6 +13,9 @@ console.log('This is an embeded script.');
 }
 
 // Variable 儲存資料的地方
+var emptyVar; // A statement the create a variable called 'nothing'
+//console.log(emptyVar); // undefined
+
 var num = 5; // A statement that creates a variable callesd "num" with a default numeric value.
 // console.log(num);
 
@@ -21,4 +24,11 @@ var x=10 , y=20 ,z=40; // Create multiple variables in one single statment
 // var x=20; // potential error
 
 x = 25;// Change the value of a variable
-console.log(x);
+//console.log(x);
+
+var name = 'Mike';// Create a variable with a string as its default value.
+var anotherName ='John';// A pair of double-quotation marks also work for a string.
+//var badQuotation = 'ah';// 引號不可混用,容器名稱不可有空格`
+
+x= 'This is a variable called "x" '; // Change the value to a different type of data; not recommended
+//console.log(x); 
