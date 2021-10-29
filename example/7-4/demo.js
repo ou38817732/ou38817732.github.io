bcba610136474d7beb2309632cc09bd8db6b533d
ou38817732 <ou38817732@gmail.com>
2021-10-29 23:19:40 +0800

    444

--- example/7-4/demo.js
+++ example/7-4/demo.js
@@ -18,5 +18,8 @@ var notANumber= '0' + '100'; // These are strings, not numbers!
 //Mixing math caculations with string combination
 var test1 ='Result:' +5 + 10;
 var test2 ='Result:' +5 * 10;
-console.log(test1);
-console.log(test2)
+//console.log(test1);
+//console.log(test2);
+
+var newString = 'I do not need the word DUMB.' - 'DUMB'; // Strings can only be added with +
+console.log(newString);//NaN= not a number
