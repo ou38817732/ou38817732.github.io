bfbbcca1c6dd7303bf2f9c6ffec4a16b4c5081f9
ou38817732 <ou38817732@gmail.com>
2021-10-29 22:06:51 +0800

    Update demo.js

--- example/7-3/demo.js
+++ example/7-3/demo.js
@@ -16,4 +16,9 @@ console.log('This is an embeded script.');
 var num = 5; // A statement that creates a variable callesd "num" with a default numeric value.
 // console.log(num);
 
-var x=10 , y=2
+var x=10 , y=20 ,z=40; // Create multiple variables in one single statment
+
+// var x=20; // potential error
+
+x = 25;// Change the value of a variable
+console.log(x);
