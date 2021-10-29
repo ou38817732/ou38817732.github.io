0358bf10a0457c8c88afceefcc0d2331537f366b
ou38817732 <ou38817732@gmail.com>
2021-10-29 20:27:32 +0800

    Update demo.js

--- example/7-3/demo.js
+++ example/7-3/demo.js
@@ -1,8 +1,8 @@
 //Single-line comment
 
-/*Multi-line comment:
-When you have a lot to say, you need this 
-*/
+/* Multi-line comment:
+   When you have a lot to say, you need this.
+ */
 
 // A "ststment" ends with a semi-colon
-console.log(' This is an embeded script.'); 
+console.log('This is an embeded script.'); 
