87e6df3cd0c2b4ac8c6d5f9f57e73723cffcb7ef
ou38817732 <ou38817732@gmail.com>
2021-11-05 11:10:19 +0800

    Update demo.js

--- example/8-2/demo.js
+++ example/8-2/demo.js
@@ -10,3 +10,8 @@ console.log(numArray[4]); // last position with a different value
 console.log(numArray); // entire array
 
 var students = [ 'Tom','Mary', 'Victor', 'John'];
+
+// mixed type of data in an array ; not recommand
+var profile = ['TY Chen','NTHU',40,180,72];
+
+console.log(profile[0]);
