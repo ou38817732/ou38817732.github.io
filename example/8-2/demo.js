867a5b2491dbf9dfe053e86dbfd1e4fa34eae774
ou38817732 <ou38817732@gmail.com>
2021-11-05 10:24:29 +0800

    Update demo.js

--- example/8-2/demo.js
+++ example/8-2/demo.js
@@ -1,1 +1,12 @@
 var numArray = [ 1,5,2,11,7];
+
+console.log(numArray[0]); // First position
+console.log(numArray[1]); // Second  position
+
+console.log(numArray[4]); // last position
+numArray[4]=18;
+console.log(numArray[4]); // last position with a different value
+
+console.log(numArray); // entire array
+
+var students = [ 'Tom','Mary', 'Victor', 'John'];
