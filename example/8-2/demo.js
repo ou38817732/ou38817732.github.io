5b57b088517acd454fcbf1690763048aee2a4193
ou38817732 <ou38817732@gmail.com>
2021-11-05 11:51:57 +0800

    Update demo.js

--- example/8-2/demo.js
+++ example/8-2/demo.js
@@ -45,3 +45,37 @@ var profile = {
 
  delete profile.gender;// Delete a key
  //console.log(profile.gender);
+
+ // A more complex profile object
+ profile = {
+ 	name: 'Richard',
+ 	age:19,
+ 	affiliation: 'NTHU',
+ 	height:170,
+ 	weight:72,
+ 	contact: {
+ 		home:258774545456, // don't call!
+ 		mobile: [909948263,44444,4644554545454], //don't call!
+ 		email:' ou38817732@gmail.com'
+ 	    }
+}; 	    
+
+// Get a key from an object that is stored in a key of another object 
+//console.log(profile.contact.home);
+//console.log(profile.contact.mobile[1]);
+
+function printMsg(msg) {
+	console.log(msg);
+ }
+
+ function simpleCalc(x,y) {
+ 	console.log(x+y);
+ }
+ 
+ var demoFuncs = { 
+ 	print:printMsg,
+ 	calc:simpleCalc
+ };
+
+
+demoFuncs.print('Message printed!');
