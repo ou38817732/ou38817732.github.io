801a8786cce83fb8fa37f457b1f69ad63a672579
ou38817732 <ou38817732@gmail.com>
2021-11-18 20:17:14 +0800

    Update demo.js

--- example/9-2/demo.js
+++ example/9-2/demo.js
@@ -1,4 +1,9 @@
 window.onload = function() {
 	let firstP = document.querySelector('div > p');
-	
+	//console.log(firstP.style);//style=css feature
+	firstP.style.backgroundColor = 'red';
+	firstP.style.color = 'white';
+
+	let allDivsPs = document.querySelectorAll('div > p');
+	allDivsPs[0].style.cssText = 'background-color: green; color: red;';
 };
