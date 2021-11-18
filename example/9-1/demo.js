81378675423bf17df38a266d265101ef973bf79b
ou38817732 <ou38817732@gmail.com>
2021-11-18 18:46:48 +0800

    Update demo.js

---
+++ example/9-1/demo.js
@@ -0,0 +1,12 @@
+//console.log('Hi');
+
+//window: The entire browser window.
+
+var winWidth = window.innerWidth;
+var winHeight = window.innerHeight;
+
+//console.log(winWidth);
+//console.log(winHeight);
+
+window.location.href= 'https://google.com';
+//window.location.pathname='/nosuchpath'; 
