b2c861f1ad90f78cb6ccc9959b00e77fdb133a27
ou38817732 <ou38817732@gmail.com>
2021-11-18 20:22:33 +0800

    Update demo.js

--- example/9-2/demo.js
+++ example/9-2/demo.js
@@ -5,5 +5,9 @@ window.onload = function() {
 	firstP.style.color = 'white';
 
 	let allDivsPs = document.querySelectorAll('div > p');
-	allDivsPs[0].style.cssText = 'background-color: green; color: red;';
+	allDivsPs[1].style.cssText = 'background-color: green; color: red;';
+
+	let allDivsPs = document.getElementsByTagName('div');
+	allDivs[4].style.cssText = ' background-color:gold; color:white;';
+	allDivs[5].style.cssText = ' background-color:gold; color:white;';
 };
