0bf29832e69038bd118c0b5be4e55b9d6c95f666
ou38817732 <ou38817732@gmail.com>
2021-11-19 11:01:16 +0800

    Update demo.js

--- example/10-1/demo.js
+++ example/10-1/demo.js
@@ -31,12 +31,14 @@ else {
 }
 
 if(2===2 && 15==10){ //&& means AND
-	alert('condition 1 is true');
+	//alert('condition 1 is true');
 }
 else if(2 ===2 || 15==10){ // || means OR
-	alert('condition2 is true');
+	//alert('condition2 is true');
 }
 else{
-	alert('Neither is ture');
+	//alert('Neither is ture');
 }
+
+document.getElementById('changeDiv1').addEventListener('click');
 };
