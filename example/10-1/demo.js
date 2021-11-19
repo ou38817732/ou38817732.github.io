cf4aaf07c30085259bb180e97d113ea485f86124
ou38817732 <ou38817732@gmail.com>
2021-11-19 10:48:18 +0800

    111

--- example/10-1/demo.js
+++ example/10-1/demo.js
@@ -16,16 +16,27 @@ else{
 
 // !==means 'not equivvqlent to '
 if(1 !==2);
-alert('true');
+//alert('true');
 }
 
+// multiple separate conditions
 if(2==='2') {
-	alert('condition is true');
+	//alert('condition is true');
 }
-else if('a'==='b') {
-	alert('condition2 is true');
+else if('a'==='a') {
+	//alert('condition2 is true');
 }
 else {
-	alert('neither is true qq');
+	//alert('neither is true qq');
+}
+
+if(2===2 && 15==10){ //&& means AND
+	alert('condition 1 is true');
+}
+else if(2 ===2 || 15==10){ // || means OR
+	alert('condition2 is true');
+}
+else{
+	alert('Neither is ture');
 }
 };
