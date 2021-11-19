72b3030ce1405423f07fe5e09b6c9cc6e8110a0d
ou38817732 <ou38817732@gmail.com>
2021-11-19 10:33:27 +0800

    4111

--- example/10-1/demo.js
+++ example/10-1/demo.js
@@ -8,9 +8,24 @@ else{
 
 //real conditions
 if(1===2){
-	alert('ture');
+	//alert('ture');
 }
 else{
-	alert('false');
+	/*else{//'else' is optional} 
+	alert('false');}*/
+
+// !==means 'not equivvqlent to '
+if(1 !==2);
+alert('true');
+}
+
+if(2===1) {
+	alert('condition is true');
+}
+else if('a'==='b') {
+	alert('condition2 is true');
+}
+else {
+	alert('neither is true qq');
 }
 };
