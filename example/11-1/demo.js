e10836410dfdf60a3631cb602a5e68d78caa8e4d
ou38817732 <ou38817732@gmail.com>
2021-11-29 14:22:44 +0800

    0000

--- example/11-1/demo.js
+++ example/11-1/demo.js
@@ -1,6 +1,9 @@
 window.onload = function() {
 	let button = document.querySelector('button');//query只會回傳第一個符合項目
-	button.addEventListener('click',function() {
+	button.addEventListener('click',function(event) {
+		// event 只是變數名稱
+		// the 'event' information is passed over by the browser
+		event.stopPropagation(); // In the 'event' object, there is a stopPropagation function.
 		alert('body clicked!');
 	});
 	let div = document.querySelector('div');
